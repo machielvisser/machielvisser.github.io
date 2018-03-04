cc056196c93f164f2cd5256bd559bcdeaa1ec1ae
Machiel Visser <machiel.visser@scarabee.com>
2018-03-04 18:15:57 +0100

    Moved existing to single page

--- js/main.js
+++ js/main.js
@@ -1,41 +1,59 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              $('html, body').animate({
+                  scrollTop: (target.offset().top - 57)
+              }, 1000, "easeInOutExpo");
+              return false;
+          }
+      }
   });
 
-  // Show the navbar when the page is scrolled up
-  var MQL = 992;
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+      duration: 1000,
+      delay: 200
+  });
+  sr.reveal('.sr-contact', {
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
+  }, 300);
 
-  //primary navigation slide-in effect
-  if ($(window).width() > MQL) {
-    var headerHeight = $('#mainNav').height();
-    $(window).on('scroll', {
-        previousTop: 0
-      },
-      function() {
-        var currentTop = $(window).scrollTop();
-        //check if user is scrolling up
-        if (currentTop < this.previousTop) {
-          //if scrolling up...
-          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-            $('#mainNav').addClass('is-visible');
-          } else {
-            $('#mainNav').removeClass('is-visible is-fixed');
-          }
-        } else if (currentTop > this.previousTop) {
-          //if scrolling down...
-          $('#mainNav').removeClass('is-visible');
-          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
-        }
-        this.previousTop = currentTop;
-      });
-  }
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+      target: '#mainNav',
+      offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+      if ($("#mainNav").offset().top > 100) {
+          $("#mainNav").addClass("navbar-shrink");
+      } else {
+          $("#mainNav").removeClass("navbar-shrink");
+      }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
