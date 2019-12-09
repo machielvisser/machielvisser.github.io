bcf7256461561e4323d1d62feb5189bb76d5cd0c
Machiel Visser <azure@machielvisser.com>
2019-12-09 18:32:31 +0100

    Upgraded gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var pkg = require('./package.json');
 var browserSync = require('browser-sync').create();
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function(done) {
 
   // Bootstrap
   gulp.src([
@@ -35,6 +35,7 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/jquery'))
 
+    done();
 });
 
 // Compile SCSS
@@ -47,7 +48,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile', function() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -58,10 +59,10 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}));
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
@@ -78,10 +79,10 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -93,7 +94,7 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', gulp.series('css', 'js', 'browserSync'), function() {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
